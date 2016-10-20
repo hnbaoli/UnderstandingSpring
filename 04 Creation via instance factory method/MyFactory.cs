321a5d59466479de334c8b01da20eb876f8f8e39
paulvanbladel <paul.vanbladel@gmail.com>
2016-10-20 17:40:44 +0200

    add sample on instance factory methods

---
+++ 04 Creation via instance factory method/MyFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingSpring
+{
+    public class MyFactory
+    {
+        public MyFactory()
+        {
+
+        }
+        public  CustomerBusinessLogic CreateInstance()
+        {
+            return new CustomerBusinessLogic();
+        }
+    }
+}
---
+++ 04 Creation via instance factory method/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Spring.Context;
+using Spring.Context.Support;
+
+namespace UnderstandingSpring
+{
+    public class CustomerBusinessLogic : IBusinessLogicLayer
+    {
+        public CustomerBusinessLogic()
+        {
+            Console.WriteLine($"Constructor {nameof(CustomerBusinessLogic)} called");
+        }
+
+        public void InitViaIOC()
+        {
+            Console.WriteLine($"InitViaIOC {nameof(CustomerBusinessLogic)}");
+        }
+
+        public void ValidateCustomer()
+        {
+            Console.WriteLine($"Validating the customer");
+        }
+    }
+
+    public interface IBusinessLogicLayer
+    {
+        void ValidateCustomer();
+    }
+
+   class Program
+    {
+        static void Main(string[] args)
+        {
+            IApplicationContext bootstrapContext = ContextRegistry.GetContext();
+
+            //alternatively the bootstrapContext can be created with a section in the App.Config file
+            //IApplicationContext bootstrapContext = new XmlApplicationContext("Application-Config.xml");
+
+
+            IBusinessLogicLayer bl = bootstrapContext.GetObject("BL") as IBusinessLogicLayer;
+            bl?.ValidateCustomer();
+
+            Console.Read();
+        }
+    }
+}
